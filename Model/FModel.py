9e3a87137c189bfd03c01196a49184f8f9d0b90a
doooodles <116247280+doooodles@users.noreply.github.com>
2021-09-18 20:39:57 +0000

    feh

--- Model/FModel.py
+++ Model/FModel.py
@@ -12,4 +12,8 @@ class FModel(nn.Module, ABC):
 
     def __init__(self, d_in, d_hid, d_class, n_layers, bi=True, dropout=0.3):
         super().__init__()
-  
+        # self.model_path = r'C:\Users\86435\Documents\work_pycharm\work_NER\chinese-xlnet-mid'
+        # self.model_path = r'/data/mgliu/transformers_model/chinese-xlnet-mid'
+        # self.model_path = r'/data/mgliu/transformers_model/roberta_chinese_clue_large'
+        self.model = BertModel.from_pretrained("clue/roberta_chinese_clue_large")
+        # self.m
