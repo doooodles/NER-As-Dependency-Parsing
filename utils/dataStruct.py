e319ba9c0ae9c81f32ada18d3624b8459555bf41
doooodles <116247280+doooodles@users.noreply.github.com>
2021-11-11 20:07:29 +0000

    .gitignore

--- utils/dataStruct.py
+++ utils/dataStruct.py
@@ -9,4 +9,13 @@ class DataStruct:
 
     def __init__(self, data):
         self.edgeLeft = data['Pos_b'].tolist()
-  
+        self.edgeRight = data['Pos_e'].tolist()
+        self.values = data['Category'].tolist()
+        self.text = [data['Text'].tolist()[0]]
+        #self.label_num = len(data)
+
+
+class MyDataProcessor:
+    def __init__(self, path: str = "./train/", count: int = 10):
+        self.preLoader = dataPreLoader(path, count)
+        self.cateDict = self.preLoader.index2
