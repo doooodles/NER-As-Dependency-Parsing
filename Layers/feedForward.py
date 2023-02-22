596935abf4a2b99322d9663601226d8327a8dae2
doooodles <116247280+doooodles@users.noreply.github.com>
2023-02-22 18:03:12 +0000

    Update composer version

--- Layers/feedForward.py
+++ Layers/feedForward.py
@@ -8,4 +8,14 @@ import torch
 class feedforwardLayer(nn.Module, ABC):
     ''' A two-feed-forward-layer module '''
 
-    def __init__(self, d_in, d_
+    def __init__(self, d_in, d_hid, dropout=0.3):
+        super().__init__()
+        self.w_1 = nn.Linear(d_in, d_hid)  # position-wise
+        self.w_2 = nn.Linear(d_hid, d_in)  # position-wise
+        self.layer_norm = nn.LayerNorm(d_in, eps=1e-6)
+        self.dropout = nn.Dropout(dropout)
+        self.dropout2 = nn.Dropout(dropout)
+        # self.para = nn.Parameter(torch.tensor([0.0]))
+
+    def forward(self, x):
+       
