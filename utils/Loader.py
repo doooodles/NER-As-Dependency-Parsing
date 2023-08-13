7e2dcc22f7cf25773b79a11e22574f163b563171
doooodles <116247280+doooodles@users.noreply.github.com>
2023-08-13 09:36:20 +0000

    Updt

--- utils/Loader.py
+++ utils/Loader.py
@@ -15,3 +15,9 @@ class dataPreLoader:
         for i in trange(count):
             # 跳过两个会报错的label文件
             if i == 30 or i == 667:
+                continue
+            temp = pd.read_csv(path + "label/" + str(i) + ".csv")
+            sentence = pd.read_csv(path + "data/" + str(i) + ".txt", names=['Text'])['Text']
+            sentence = ''.join(list(sentence))
+            temp['Text'] = sentence[:min(len(sentence), 500)]
+            temp = temp[temp["Pos_b"] < 500][temp["
